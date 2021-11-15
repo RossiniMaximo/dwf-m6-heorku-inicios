984d3d195ba61223a3d094d50213088132a1b0d8
Maximo <maximorossini2016@gmail.com>
2021-11-15 14:55:28 -0300

    first-commit

---
+++ index.js
@@ -0,0 +1,13 @@
+"use strict";
+exports.__esModule = true;
+var express = require("express");
+var app = express();
+var port = process.env.PORT || 3000;
+app.get("/hola", function (req, res) {
+    res.json({
+        message: "Hola soy el mensaje del server"
+    });
+});
+app.listen(3000, function () {
+    console.log("Hola soy express" + "" + port);
+});
---
+++ index.ts
@@ -0,0 +1,13 @@
+import * as express from "express";
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/hola", (req, res) => {
+    res.json({
+        message: "Hola soy el mensaje del server"
+    })
+})
+
+app.listen(3000, () => {
+    console.log("Hola soy express" + "" + port);
+})
