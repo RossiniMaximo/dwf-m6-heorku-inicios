4462bfcd534cfda8b492a89dc2263da167c12e8c
Maximo <maximorossini2016@gmail.com>
2021-11-15 17:13:49 -0300

    env bars

--- index.ts
+++ index.ts
@@ -5,9 +5,17 @@ const port = process.env.PORT || 3000;
 app.get("/hola", (req, res) => {
     res.json({
         message: "Hola soy el mensaje del server,como te va"
-    })
-})
+    });
+});
+
+process.env.NODE_ENV = "Development"
+
+app.get("/env", (req, res) => {
+    res.json({
+        environment: process.env.NODE_ENV,
+    });
+});
 
 app.listen(3000, () => {
     console.log("Hola soy express" + port);
-})
+});
