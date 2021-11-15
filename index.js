e05a1e8ad1e5f24679442dee32e9979682a61768
Maximo <maximorossini2016@gmail.com>
2021-11-15 15:31:12 -0300

    sixth-commit

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ var app = express();
 var port = process.env.PORT || 3000;
 app.get("/hola", function (req, res) {
     res.json({
-        message: "Hola soy el mensaje del server,como te va ?"
+        message: "Hola soy el mensaje del server,como te va"
     });
 });
 app.listen(3000, function () {
-    console.log("Hola soy express" + "" + port);
+    console.log("Hola soy express" + port);
 });
