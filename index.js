c6c36944c74a6dbc95e31427635a1eea1302cd1c
Maximo <maximorossini2016@gmail.com>
2021-11-15 15:07:40 -0300

    cambio

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var app = express();
 var port = process.env.PORT || 3000;
 app.get("/hola", function (req, res) {
     res.json({
-        message: "Hola soy el mensaje del server"
+        message: "Hola soy el mensaje del server,como te va ?"
     });
 });
 app.listen(3000, function () {
